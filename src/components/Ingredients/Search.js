1cc5996e4149f472b908fe2bff5a818e9c457262
Denizhan Gönen <denizhangonen@Denizhans-MacBook-Pro.local>
2020-01-04 21:58:29 +0300

    search completed for title property

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
 const Search = React.memo(props => {
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState("");
+
+  useEffect(() => {
+    const query =
+      enteredFilter.length === 0
+        ? ""
+        : `?orderBy="title"&equalTo="${enteredFilter}"`;
+
+    fetch("https://react-hooks-b3c78.firebaseio.com/ingredients.json" + query)
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(responseData => {
+        console.log(responseData);
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        onLoadIngredients(loadedIngredients);
+      });
+  }, [enteredFilter, onLoadIngredients]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredFilter}
+            onChange={event => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
