f22823d7c4860a892cae02e7a863a8e993530282
Denizhan Gönen <denizhangonen@Denizhans-MacBook-Pro.local>
2020-01-05 20:24:24 +0300

    authcontext added for authorization

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState, useEffect, useCallback } from "react";
+import React, { useReducer, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
