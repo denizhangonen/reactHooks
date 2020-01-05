45d21324efa413e4d5bfe3a527cf5892091fc4d3
Denizhan Gönen <denizhangonen@Denizhans-MacBook-Pro.local>
2020-01-05 18:02:12 +0300

    useReducer added for ingredients manipulations

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,12 +1,26 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useReducer, useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
 
+const ingredientsReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients;
+    case "ADD":
+      return [...currentIngredients, action.ingredient];
+    case "REMOVE":
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default:
+      throw new Error("This is no man s land!");
+  }
+};
+
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientsReducer, []);
+  // const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
@@ -22,10 +36,14 @@ const Ingredients = () => {
         return response.json();
       })
       .then(responseData => {
-        setUserIngredients(prevIngredients => [
-          ...prevIngredients,
-          { id: responseData.name, ...ingredient }
-        ]);
+        // setUserIngredients(prevIngredients => [
+        //   ...prevIngredients,
+        //   { id: responseData.name, ...ingredient }
+        // ]);
+        dispatch({
+          type: "ADD",
+          ingredient: { id: responseData.name, ...ingredient }
+        });
       });
   };
 
@@ -37,9 +55,10 @@ const Ingredients = () => {
     })
       .then(() => {
         setIsLoading(false);
-        setUserIngredients(prevIngredients =>
-          prevIngredients.filter(i => i.id.toString() !== id.toString())
-        );
+        // setUserIngredients(prevIngredients =>
+        //   prevIngredients.filter(i => i.id.toString() !== id.toString())
+        // );
+        dispatch({ type: "REMOVE", id: id });
       })
       .catch(error => {
         setError("Something Went Wrong: " + error.message);
@@ -48,7 +67,8 @@ const Ingredients = () => {
   };
 
   const filteredIngredientsHandler = useCallback(ingredients => {
-    setUserIngredients(ingredients);
+    // setUserIngredients(ingredients);
+    dispatch({ type: "SET", ingredients: ingredients });
   }, []);
 
   const removeError = () => {
